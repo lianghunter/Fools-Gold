37354cf47f5cf68199f5be65c40ec9fff0abe411
Patk4321 <patk4321@gmail.com>
2024-04-20 16:28:28 -0600

    FIXED EVERYTING!!!! optput dynamic text

---
+++ Madder3/Assets/logicScript.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class logicScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+ public string outputText;
+
+
+
+}
